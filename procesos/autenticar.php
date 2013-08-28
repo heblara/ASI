3589510dba18e155a4ddf7e7ea2db6b99e86a003
Heber <heberlara07@gmail.com>
2013-08-28 02:50:50 -0600

    Reparacion del autenticar.php

--- procesos/autenticar.php
+++ procesos/autenticar.php
@@ -9,16 +9,18 @@ if($user=="" || $user==null || trim($user)=="" || $pwd=="" || $pwd==null || trim
     //echo "Combinaci&oacute;n de usuario y contrase&ntilde;a incorrecta";
     //header("Location:?mod=login&msj=1");
 }else{
-    $objUser=new CargoMania;
-    $log=array($user,$pwd);
-    $consultarUsuario=$objUser->consultar_usuario($log);
-    $c=$consultarUsuario->rowCount();
-    if($c==1){ //Verificando que exista ese usuario y este activo
-        $usuario=$consultarUsuario->fetch(PDO::FETCH_OBJ);
-        //session_start();
-        $_SESSION["tipo"]=$usuario->TipoUsuario;
-        $_SESSION["nombre"]=$usuario->Usuario;
+    //$objUser=new CargoMania;
+    //$log=array($user,$pwd);
+    //$consultarUsuario=$objUser->consultar_usuario($log);
+    //$c=$consultarUsuario->rowCount();
+    $conuser=pg_query("select * from usuario where Usuario='".$user."' and Contrasena=md5('".$pwd."')");
+    if(pg_num_rows($conuser)==1){ //Verificando que exista ese usuario y este activo
+        //$usuario=$consultarUsuario->fetch(PDO::FETCH_OBJ);
+        //pg_fetch_assoc($conuser);
         $_SESSION["autenticado"]="si";
+        //session_start();
+        /*$_SESSION["tipo"]=$usuario->TipoUsuario;
+        $_SESSION["nombre"]=$usuario->Usuario;*/
         //echo "Usuario encontrado";
         //$_SESSION["tipo"]=$usuario->TipoUsuario;
         $respuesta->mensaje = 3;
